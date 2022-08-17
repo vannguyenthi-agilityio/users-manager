ae2dcfcbfa9159e9bc798f78e442bdf0e488b1ea
Ni Ngo <ni.ngo@asnet.com.vn>
2022-08-17 15:50:02 +0700

    Config path and add sample for buttons

---
+++ app/src/themes/colors.ts
@@ -0,0 +1,11 @@
+const COLORS = {
+  foregroundColor: "#fff",
+  backgroundColor: "#000",
+
+  btnBgColor: '#3182ce',
+  btnTxtColor: '#fff',
+  btnSuccess: '#68d391',
+  btnDanger: '#fc8181',
+}
+
+export default COLORS;
---
+++ app/src/themes/metrics.ts
@@ -0,0 +1,20 @@
+const METRICS = {
+  zIndex: {
+    base: 0,
+    sm: 1,
+    md: 2,
+    lg: 3,
+    modal: 10
+  },
+  opacity: {
+    tiny: 0,
+    base: 0.5,
+    lg: 1
+  },
+  borderRadius: {
+    base: '5px',
+    round: '50%'
+  }
+}
+
+export default METRICS
