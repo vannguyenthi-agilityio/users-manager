f0218fc57ebfad4769cc84412b53ce48e78720ba
Huy Chau <huy.chau@asnet.com.vn>
2022-08-22 23:17:18 +0700

    refactor: styles variables and configs

---
+++ app/src/themes/base.ts
@@ -0,0 +1,52 @@
+const THEME_BASE = {
+  // Colors
+  colorPrimary: '#f0c13b',
+  colorSecondary: '#241e35',
+  colorDark: '#1c1825',
+  colorBlack: '#000',
+  colorWhite: '#fff',
+  colorSuccess: '#68d391',
+  colorError: '#fc8181',
+
+  // Fonts
+  fontFamilyPrimary: 'Lato, sans-serif',
+  fontFamilySecondary: 'Roboto, sans-serif',
+
+  fontSizeXs: '12px',
+  fontSizeSm: '14px',
+  fontSizeBase: '16px',
+  fontSizeMd: '18px',
+  fontSizeLg: '20px',
+
+  fontWeightNormal: 400,
+  fontWeightMedium: 500,
+  fontWeightBold: 700,
+
+  // Z-indexes
+  zIndexHide: -1,
+  zIndexAuto: 'auto',
+  zIndexBase: 0,
+  zIndexDropdown: 1000,
+  zIndexOverlay: 1100,
+  zIndexModal: 1200,
+  zIndexTooltip: 1300,
+
+  // Border radius
+  borderRadiusNone: '0',
+  borderRadiusSm: '5px',
+  borderRadiusBase: '10px',
+  borderRadiusMd: '12px',
+  borderRadiusRound: '50%',
+
+  // Sizes
+  sizeFull: '100%',
+  sizeSm: '375px',
+  sizeMd: '768px',
+  sizeLg: '1024px',
+  sizeXl: '1915px',
+
+  paddingSm: '16px',
+  paddingMd: '30px'
+};
+
+export default THEME_BASE;
