63fdc0d9f6a5931c20e0f755b00a66f5197f06bd
Kien Dinh <kien.dinh@asnet.com.vn>
2022-09-09 11:13:12 +0700

    feat: update components input group

--- app/src/themes/base.ts
+++ app/src/themes/base.ts
@@ -10,7 +10,7 @@ const THEME_BASE = {
   colorBackground: '#1d172b',
   colorText: '#fff',
   colorBgFooterDetail: '#151515',
-  colorInputGroup: '#191524',
+  colorInputDark: '#191524',
 
   colorPlaceholder: 'rgba(26, 24, 38, 25%)',
   colorPlaceholderLight: 'rgba(255, 255, 255, 25%)',
