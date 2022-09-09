db8d8cf4b3f078ed2e3c0562b925c657c21d5cd2
Ni Ngo <ni.ngo@asnet.com.vn>
2022-09-09 19:43:32 +0700

    feat: add constants for errors and routes

--- app/src/constants/common.ts
+++ app/src/constants/common.ts
@@ -2,3 +2,7 @@ export const PHONE = {
   pattern: '[0-9]{3}-[0-9]{3}-[0-9]{4}',
   maxLength: '14'
 };
+
+export const LOCAL_STORAGE_KEYS = {
+  USER_INFO: 'userInfo'
+};
---
+++ app/src/constants/routes.ts
@@ -0,0 +1,6 @@
+export const ROUTES = {
+  HOME: '/',
+  REFERRAL: '/referral',
+  LOGIN: '/login',
+  NOT_FOUND: '/404'
+};
