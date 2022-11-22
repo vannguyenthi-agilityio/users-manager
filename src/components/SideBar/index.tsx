0ab13a0cfc39b704533fefb1ede2da4531b8e552
Van Nguyen <van.nguyenthi@asnet.com.vn>
2022-11-22 07:40:01 +0700

    feat: update to fix show storybook

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -95,6 +95,7 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => (
     w={{ base: 'full', md: 60 }}
     pos="fixed"
     h="full"
+    pr={10}
     {...rest}
   >
     <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
